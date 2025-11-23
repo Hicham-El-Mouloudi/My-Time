4d5dad1b55434bf4de82976e3f946d8b23fdbbf0
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-11-23 19:44:25 +0100

    feat: Implement initial `MainActivity` with bottom navigation and fragment switching.

--- app/src/main/java/com/ensao/mytime/MainActivity.java
+++ app/src/main/java/com/ensao/mytime/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
+import com.ensao.mytime.alarm.AlarmFragment;
 import com.ensao.mytime.settings.SettingsFragment;
+import com.ensao.mytime.sleep.SleepSessionFragment;
+import com.ensao.mytime.statistics.StatisticsFragment;
+import com.ensao.mytime.study.StudySessionFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,10 +33,36 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Setting the settings fragment
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.main, new SettingsFragment())
-                .commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            navigateTo(item.getItemId());
+            return true;
+        });
+
+        if (savedInstanceState == null) {
+            bottomNavigationView.setSelectedItemId(R.id.nav_alarm);
+        }
+    }
+
+    private void navigateTo(int itemId) {
+        Fragment fragment = null;
+        if (itemId == R.id.nav_alarm) {
+            fragment = new AlarmFragment();
+        } else if (itemId == R.id.nav_sleep_session) {
+            fragment = new SleepSessionFragment();
+        } else if (itemId == R.id.nav_study_session) {
+            fragment = new StudySessionFragment();
+        } else if (itemId == R.id.nav_statistics) {
+            fragment = new StatisticsFragment();
+        } else if (itemId == R.id.nav_settings) {
+            fragment = new SettingsFragment();
+        }
+
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainContent, fragment)
+                    .commit();
+        }
     }
 }
