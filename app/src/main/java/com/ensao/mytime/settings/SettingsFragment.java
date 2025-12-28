20607ad29524586fc6f21e26d612e8a55d47a45d
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-12-28 16:27:21 +0100

    feat: Adding basic Internationalization to the xml views

--- app/src/main/java/com/ensao/mytime/settings/SettingsFragment.java
+++ app/src/main/java/com/ensao/mytime/settings/SettingsFragment.java
@@ -6,10 +6,32 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import com.ensao.mytime.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements android.content.SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(android.content.SharedPreferences sharedPreferences, String key) {
+        if ("language".equals(key)) {
+            String lang = sharedPreferences.getString(key, "en");
+            LocaleHelper.setLocale(requireContext(), lang);
+            requireActivity().recreate();
+        }
     }
 }
